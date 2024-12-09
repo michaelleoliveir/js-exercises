928f0ee78cd29a95838e61cad2bb5f29e6ff83db
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2024-12-09 18:28:34 -0300

    improve: Calculo da tabuada

--- exercicio-3/ex03.js
+++ exercicio-3/ex03.js
@@ -1,2 +1,33 @@
+let telaInicio = document.getElementById("inicio")
+let telaTabuada = document.getElementById("tabuada")
 let num = document.getElementById("numero")
-console.log(num)
+let listaTabuada = document.getElementById("lista")
+let aviso = document.getElementById("aviso")
+
+function calcular() {
+    telaInicio.style.display = "none"
+    telaTabuada.style.display = "flex"
+    numero = num.value
+
+    if (numero === "") {
+        aviso.style.display = "flex"
+        num.style.borderColor = "#cf6060"
+        voltar();
+        return;
+    } else {
+        for(let i = 0; i <= 10; i++) {
+            let item = document.createElement("li");
+            item.textContent = `${numero} x ${i} = ${numero * i}`;
+            listaTabuada.appendChild(item);
+        }
+
+        aviso.style.display = "none"
+    }
+}
+
+function voltar() {
+    telaInicio.style.display = "flex"
+    telaTabuada.style.display = "none"
+    num.value = ""
+    listaTabuada.innerHTML = ""
+}
