48777305a981cbe6e3bf0c81dd2d06351acc6bad
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2024-12-30 10:47:06 -0300

    feat: finalizando o exercício 08

--- exercicio-8/ex08.js
+++ exercicio-8/ex08.js
@@ -8,7 +8,22 @@ filterElement.addEventListener("input", filtrarPesquisa);
 function filtrarPesquisa() {
     if(filterElement.value !== '') {
         for(let card of cards) {
-            let titulo = card.querySelector("h2")
+
+            // passando os elementos para lower case
+            let titulo = card.querySelector("h2").textContent.toLowerCase();
+            let pesquisa = filterElement.value.toLowerCase();
+
+            // caso o título contenha o texto inserido na pesquisa, o display dos cards muda
+            if(!titulo.includes(pesquisa)) {
+                card.style.display = "none"
+            }
+            else {
+                card.style.display = "block"
+            }
+        }
+    } else {
+        for(let card of cards) {
+            card.style.display = "block"
         }
     }
 }
